28c1f8d26a8be88c79f9fcafcee05651067b2589
tinojanwebdev <tamilmarantwebdep@gmail.com>
2025-08-12 10:24:58 -0700

    .env DB-PASS

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const db = mysql.createConnection({
 console.log({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });
