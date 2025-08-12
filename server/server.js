373ce1e28bdadfa1baad19e762479636c5208083
tinojanwebdev <tamilmarantwebdep@gmail.com>
2025-08-12 11:50:18 -0700

    .env port

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'Make12Admin1',  // <-- changed here to DB_PASSWORD
   database: process.env.DB_NAME || 'globehub',
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT || 3306,
 });
 
 
